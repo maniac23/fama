400ea339908fa8c1d2ce2b938620a5c3bed0716d
Mikhail Tukai <mikhail@imaguru.by>
2016-12-19 14:05:13 +0300

    added initial structure & menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+//= ../../node_modules/jquery/dist/jquery.min.js
+//= ../../node_modules/fullpage.js/dist/jquery.fullpage.min.js
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+      navigation: true,
+      navigationPosition: 'left',
+      css3: true,
+      easingcss3: 'ease-in-out',
+      scrollingSpeed: 1000,
+    });
+});
