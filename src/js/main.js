c80c810dd7e38f92336d31e6df2ad4c07bccca7f
Mikhail Tukai <mikhail@imaguru.by>
2016-12-19 21:19:51 +0300

    added script for scrolling device frame 🔨

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,25 @@
 //= ../../node_modules/jquery/dist/jquery.min.js
 //= ../../node_modules/fullpage.js/dist/jquery.fullpage.min.js
 
+
+//
 $(document).ready(function() {
+// initialize fullscreen scroll
     $('#fullpage').fullpage({
       navigation: true,
       navigationPosition: 'left',
       css3: true,
       easingcss3: 'ease-in-out',
       scrollingSpeed: 1000,
+      scrollBar: true,
+      verticalCentered: true,
     });
-});
+  });
+// check if first-screen is visible & remove device frame from viewport
+  $(window).scroll(function(){
+    if($('.first-screen').hasClass('active')){
+      $('.device').removeClass('device--visible');
+    } else {
+        $('.device').addClass('device--visible');
+    }
+    });
