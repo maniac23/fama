6741c5414fa612847e0bbceab0ef90b95ec9610e
Mikhail Tukai <mikhail@imaguru.by>
2016-12-20 13:56:10 +0300

    added fifth screen 🔨

--- src/js/main.js
+++ src/js/main.js
@@ -2,29 +2,31 @@
 //= ../../node_modules/jquery/dist/jquery.min.js
 //= ../../node_modules/fullpage.js/dist/jquery.fullpage.min.js
 
-
 //
 $(document).ready(function() {
-// initialize fullscreen scroll
-    $('#fullpage').fullpage({
-      navigation: true,
-      navigationPosition: 'left',
-      css3: true,
-      easingcss3: 'ease-in-out',
-      scrollingSpeed: 1000,
-      scrollBar: true,
-      verticalCentered: true,
-      onLeave: function(index, nextIndex, direction){
-            var leavingSection = $(this);
-            console.log(index);
-          }
-    });
-  });
-// check if first-screen is visible & remove device frame from viewport
-  $(window).scroll(function(){
-    if($('.first-screen').hasClass('active')){
-      $('.device').removeClass('device--visible');
-    } else {
-        $('.device').addClass('device--visible');
+  // initialize fullscreen scroll
+  $('#fullpage').fullpage({
+    navigation: true,
+    navigationPosition: 'left',
+    css3: true,
+    easingcss3: 'ease-in-out',
+    scrollingSpeed: 1000,
+    scrollBar: true,
+    verticalCentered: true,
+    onLeave: function(index, nextIndex, direction) {
+      var leavingSection = $(this);
+      console.log(index);
     }
-    });
+  });
+});
+// check if first-screen is visible & remove device frame from viewport
+$(window).scroll(function() {
+  if ($('.first-screen').hasClass('active')) {
+    $('.device').removeClass('device--visible');
+  } else if ($('.fifth-screen').hasClass('active')) {
+    $('.device').addClass('device--full-visible');
+  } else {
+    $('.device').removeClass('device--full-visible');
+    $('.device').addClass('device--visible');
+  }
+});
