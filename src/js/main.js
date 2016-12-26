506a35d674e951d436ced21cb3d5397907ffacc6
Mikhail Tukai <tukaym@gmail.com>
2016-12-26 21:28:49 +0300

    fixed slides animation

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     menu: '#menu',
     navigation: true,
     navigationPosition: 'left',
-    anchors:['famaapp', 'revista', 'tests', 'autographs', 'descargar'],
+    anchors: ['famaapp', 'revista', 'tests', 'autographs', 'descargar'],
     css3: true,
     easingcss3: 'ease-in-out',
     scrollingSpeed: 1000,
@@ -17,12 +17,15 @@ $(document).ready(function() {
     // verticalCentered: true,
     onLeave: function(index, nextIndex, direction) {
       var leavingSection = $(this);
-      console.log(index);
+      console.log(nextIndex);
+      if(nextIndex == 1) {
+        $('.device').removeClass('device--visible');
+      }
     }
   });
 
   // toggle mobile-menu
-  $('.menu-icon').click(function(){
+  $('.menu-icon').click(function() {
     $(this).toggleClass('menu-icon--opened');
     $('.mobile-menu').fadeToggle().css('display', 'flex');
     $('#fp-nav').fadeToggle();
@@ -30,7 +33,7 @@ $(document).ready(function() {
 
   // hide mobile menu on link click
 
-  $('.mobile-menu a, .mobile-menu button').click(function(){
+  $('.mobile-menu a, .mobile-menu button').click(function() {
     $('.menu-icon').removeClass('menu-icon--opened');
     $('.mobile-menu').fadeOut();
   });
@@ -39,18 +42,17 @@ $(document).ready(function() {
   // check if first-screen is visible & remove device frame from viewport
   $(window).scroll(function() {
     if ($('.first-screen').hasClass('active')) {
-      $('.device').removeClass('device--visible');
+      $('.device').removeClass('device--visible, device--full-visible');
     } else if($('.second-screen').hasClass('active')) {
       $('.device').addClass('device--visible');
       $('.device__slide').removeClass('device__slide--visible');
-      // $("#slide2").addClass("device__slide--visible");
     } else if($('.third-screen').hasClass('active')) {
       $('.device__slide').removeClass('device__slide--visible');
-      // $('.device').addClass('device--visible');
-      $("#slide2").addClass("device__slide--visible");
-    } else if ($('.fourth-screen').hasClass('active')){
-      $('.device__slide').removeClass('device__slide--visible');
+      $('#slide2').addClass('device__slide--visible');
+    } else if ($('.fourth-screen').hasClass('active')) {
+      $('#slide4').removeClass('device__slide--visible');
       $('#slide3').addClass('device__slide--visible');
+      $('.device').removeClass('device--full-visible');
     }
     else if ($('.fifth-screen').hasClass('active')) {
       $('.device').addClass('device--full-visible');
@@ -58,9 +60,10 @@ $(document).ready(function() {
     }
     else {
       $('.device').removeClass('device--full-visible');
-      $('.device').addClass('device--visible');
+      $('.device').removeClass('device--visible');
     }
   });
+
   // open hero-form
   $('button[data-id="hero"]').click(function() {
     $('#hero-form').fadeIn().css('display', 'flex');
